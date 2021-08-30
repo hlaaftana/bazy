742e91f2a1b6e7cdd2b907b5d660de7faf301cf4
hlaaftana <hlaaftana@gmail.com>
2021-08-30 11:43:04 +0300

    uhhh

--- src/brotheracademia.nim
+++ src/brotheracademia.nim
@@ -1,3 +1,3 @@
-import brotheracademia/[parser, runtime]
+import brotheracademia/[parser, expressions, runtime]
 
-
+echo parse("combination(n: Int, r: Int) = \\for result x = 1, each i in 0..<r do while i < r do x = x * (n - i) / (r - i)")
